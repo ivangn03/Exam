16851ec5174e06a37216fb61241dec70a2902324
Ivan <ivan.gn03@gmail.com>
2019-10-26 21:35:27 +0300

    Removed crutch

--- DAL/Models/Frameworks.cs
+++ DAL/Models/Frameworks.cs
@@ -12,15 +12,5 @@ namespace DAL.Models
         public string Name { get; set; }
         public string Language { get; set; }
         public string ImageURL { get; set; }
-        public static IEnumerable<Framework> GetFrameworks()
-        {
-            return new List<Framework>
-            {
-                new Framework{Id = 1, Name = "React", Language = "JS", ImageURL="https://hackernoon.com/hn-images/1*y6C4nSvy2Woe0m7bWEn4BA.png"},
-                new Framework{Id = 2, Name = "Angular", Language = "JS", ImageURL="https://vegibit.com/wp-content/uploads/2018/11/AngularJS.png"},
-                new Framework{Id = 3, Name = "Vue", Language = "JS", ImageURL="https://miro.medium.com/max/1200/1*-PlqbnwqjqJi_EVmrhmuDQ.jpeg"},
-                new Framework{Id = 4, Name = "Flutter", Language = "Dart", ImageURL="https://upload.wikimedia.org/wikipedia/commons/1/17/Google-flutter-logo.png"},
-            };
-        }
     }
 }
