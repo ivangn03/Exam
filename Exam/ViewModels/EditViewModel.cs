39af0da553b0846cea6817c7901a8ca718113963
Ivan <ivan.gn03@gmail.com>
2019-10-29 17:40:35 +0200

    UI changes
    Return button added.

--- Exam/ViewModels/EditViewModel.cs
+++ Exam/ViewModels/EditViewModel.cs
@@ -1,22 +1,28 @@
-﻿using Exam.Models;
+﻿using Exam.Infrastucture;
+using Exam.Models;
 using Exam.Views;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Exam.ViewModels
 {
     class EditViewModel
     {
         public FrameworkUIModel Framework { get; set; }
+        public ICommand BackToMain { get; set; }
         public EditViewModel(FrameworkUIModel framework)
         {
             this.Framework = framework;
             EditView editView = new EditView {
                 DataContext = this
             };
+            BackToMain = new RelayCommand(x => {
+                editView.Close();
+            });
             editView.ShowDialog();
         }
     }
